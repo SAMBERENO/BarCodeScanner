1f5c3c93af6b2a16fe8f166e3f70ab247f727f97
SAMBERENO <arek4@spoko.pl>
2026-08-18 18:14:36 +0200

    Added GetRecordByCode.ts

---
+++ src/functions/GetRecordByCode.ts
@@ -0,0 +1,61 @@
+import { getBackendUrl } from "./ConnectToService";
+
+export type RecordsJson = {
+    zmiana: string;
+    nrWyrobu: string;
+    nrZleceniaiPudla: string;
+    dataProdukcji: string;
+    sumaUszczelek: number;
+    sumaBrakow: number;
+    niezgodnosci: number;
+    kz: number;
+    braki: {
+        A: number;
+        B: number;
+        C: number;
+        D: number;
+        E: number;
+        F: number;
+        G: number;
+        H: number;
+        I: number;
+        J: number;
+        K: number;
+        L: number;
+        M: number;
+        N: number;
+        O: number;
+        P: number;
+        R: number;
+        S: number;
+        T: number;
+        U: number;
+        V: number;
+        W: number;
+        X: number;
+    };
+};
+
+export async function getRecordByCode(
+    scannedCode: string
+): Promise<RecordsJson> {
+
+    const backendUrl = await getBackendUrl();
+
+    const response = await fetch(
+        `${backendUrl}/android/record/${encodeURIComponent(scannedCode)}`,
+        {
+            method: "GET"
+        }
+    );
+
+    if (!response.ok) {
+        throw new Error(
+            `Nie udało się pobrać pozycji: ${response.status}`
+        );
+    }
+
+    const record: RecordsJson = await response.json();
+
+    return record;
+}
